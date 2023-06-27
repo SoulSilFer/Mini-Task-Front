cab299d9ef2e0dac2401b1544a25ec7d92b85308
Vitor Fernandes <79426743+SoulSilFer@users.noreply.github.com>
2023-06-27 12:31:22 -0300

    feat: dev backend url

--- src/core/infra/http/api-url-factory.ts
+++ src/core/infra/http/api-url-factory.ts
@@ -1,4 +1,5 @@
-const BACKEND_BASEURL = 'http://localhost:3000';
+// const BACKEND_BASEURL = 'http://localhost:3000';
+const BACKEND_BASEURL = 'nodejs-production-c858.up.railway.app';
 
 export const makeApiURL = (path: string): string =>
   `${BACKEND_BASEURL}/${path}`;
